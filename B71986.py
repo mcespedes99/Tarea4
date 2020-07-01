84e3d2210aa6aed86c46dc8c3c233db47baceacc
Mauricio Céspedes <mcespedes99@gmail.com>
2020-07-01 16:57:40 -0600

    Commit 2

--- B71986.py
+++ B71986.py
@@ -21,8 +21,53 @@ print("Estudiante: Mauricio Céspedes Tenorio. Carné: B71986.")
 #Extracción de datos del bits10k.cvs en un array que serían los bits:
 bits = np.genfromtxt('bits10k.csv', delimiter='\n')
 
-# Frecuencia de operación
-f = 1000
+#Número de bit:
+N = len(bits)
 
-# Duración del período de cada onda
+# Frecuencia de portadora:
+f = 5000
+
+# Periodo de símbolo igual a periodo completo de onda portadora:
 T = 1/f
+
+# Número de puntos de muestreo por cada período:
+p = 55
+
+# Creación de espacio lineal para después crear onda sinusoidal:
+tp = np.linspace(0, T, p)
+
+#Ondas necesarias para modulación BPSK:
+# 1. Creación de la forma de onda sinusoidal:
+seno = np.sin(2*np.pi * f * tp)
+
+#Definición de frecuencia de muestre:
+fm = p/T #55 puntos por periodo
+
+# Creación de la línea temporal para toda la señal a modular:
+t = np.linspace(0, N*T, N*p)
+
+#Creación de array de señal a modular. Iniclalmente llena de ceros:
+senal = np.zeros(N*p)
+
+# Creación de la señal modulada BPSK:
+for i, b in enumerate(bits):
+  senal[i*p:(i+1)*p] = (2*b-1) * seno #El término (2*b-1) hace que los 1 en bits sigan siendo 1 y los 0 pasen a -1. De esta forma, en los ceros se coloca un seno negativo y en los 1 un seno positivo.
+
+#Visualización de los primeros 6 bits modulados
+pb = 6
+plt.figure()
+plt.plot(senal[0:pb*p], label='Señal modulada en BPSK')
+plt.xlabel('Puntos de muestreo (50 por bit o periodo)')
+plt.ylabel('Amplitud')
+plt.legend()
+plt.show()
+
+'''
+2. Calcular la potencia promedio de la señal modulada generada.
+'''
+# Potencia intantánea de la señal modulada:
+Pinst = senal**2
+
+# Potencia promedio: promedio temporal (equivalente a la integración) de la potencia instantánea
+Pprom = integrate.trapz(Pinst, t) / (T*N)
+print("La potencia promedio de la señal modulada es: "+"{:.4f}".format(Pprom))
